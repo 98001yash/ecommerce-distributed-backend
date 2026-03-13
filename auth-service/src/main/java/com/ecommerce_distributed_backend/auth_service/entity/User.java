427619deb6aa572d8167a3beeedae6a9efe3efbc
98001yash <yashchauhan.gaya@gmail.com>
2026-03-13 07:14:28 +0530

    User entity created

---
+++ auth-service/src/main/java/com/ecommerce_distributed_backend/auth_service/entity/User.java
@@ -0,0 +1,66 @@
+package com.ecommerce_distributed_backend.auth_service.entity;
+
+
+import com.ecommerce_distributed_backend.auth_service.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_user_email", columnList = "email"),
+                @Index(name = "idx_user_role", columnList = "role"),
+                @Index(name = "idx_user_created_at", columnList = "created_at")
+        }
+)
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "is_enabled")
+    private boolean isEnabled = true;
+
+    @Column(name = "is_account_non_locked")
+    private boolean isAccountNonLocked = true;
+
+    @Column(name = "is_credentials_non_expired")
+    private boolean isCredentialsNonExpired = true;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ auth-service/src/main/java/com/ecommerce_distributed_backend/auth_service/entity/enums/Role.java
@@ -0,0 +1,8 @@
+package com.ecommerce_distributed_backend.auth_service.entity.enums;
+
+public enum Role {
+
+    CUSTOMER,
+    ADMIN,
+    SELLER
+}

62c8edb8efb26e58756ed45f4b65233863b16f4b
98001yash <yashchauhan.gaya@gmail.com>
2026-03-13 08:25:57 +0530

    added the auth service onterface

---
+++ auth-service/src/main/java/com/ecommerce_distributed_backend/auth_service/service/AuthService.java
@@ -0,0 +1,21 @@
+package com.ecommerce_distributed_backend.auth_service.service;
+
+import com.ecommerce_distributed_backend.auth_service.dtos.request.LoginRequest;
+import com.ecommerce_distributed_backend.auth_service.dtos.request.LogoutRequest;
+import com.ecommerce_distributed_backend.auth_service.dtos.request.RefreshTokenRequest;
+import com.ecommerce_distributed_backend.auth_service.dtos.request.RegisterRequest;
+import com.ecommerce_distributed_backend.auth_service.dtos.response.AuthResponse;
+import com.ecommerce_distributed_backend.auth_service.dtos.response.TokenValidationResponse;
+
+public interface AuthService {
+
+
+    AuthResponse register(RegisterRequest request);
+
+    AuthResponse login(LoginRequest request);
+    TokenValidationResponse validateToken(String token);
+
+    AuthResponse refreshToken(RefreshTokenRequest request);
+
+    void logout(LogoutRequest request);
+}

591b40010e63fad2e6d97ffecbe309e4562ff2ed
98001yash <yashchauhan.gaya@gmail.com>
2026-03-13 14:25:11 +0530

    added the product mioroservice

---
+++ product-service/src/main/java/com/ecommerce_distributed_backend/product_service/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce_distributed_backend.product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}

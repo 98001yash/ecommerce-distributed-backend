603ccb078699feceb56b9110da06d7e4170de4af
98001yash <yashchauhan.gaya@gmail.com>
2026-03-17 16:45:43 +0530

    added the order not found exception classs

--- order-service/src/main/java/com/ecommerce_distributed_system/order_service/exception/OrderAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce_distributed_system.order_service.exception;
-
-public class OrderAlreadyExistsException extends RuntimeException {
-    public OrderAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- order-service/src/main/java/com/ecommerce_distributed_system/order_service/exception/OrderNotFoundException.java
+++ order-service/src/main/java/com/ecommerce_distributed_system/order_service/exception/OrderNotFoundException.java
@@ -3,7 +3,7 @@ package com.ecommerce_distributed_system.order_service.exception;
 public class OrderNotFoundException extends RuntimeException {
 
 
-    public OrderNotFoundException(String message) {
-        super(message);
+    public OrderNotFoundException(String orderId) {
+        super("Order not found with id: "+orderId);
     }
 }

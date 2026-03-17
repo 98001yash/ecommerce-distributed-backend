6644be38bf75731457e1f4f896990bd027fe5463
98001yash <yashchauhan.gaya@gmail.com>
2026-03-17 17:08:10 +0530

    created the order repositorty

---
+++ order-service/src/main/java/com/ecommerce_distributed_system/order_service/repository/OrderRepository.java
@@ -0,0 +1,34 @@
+package com.ecommerce_distributed_system.order_service.repository;
+
+import com.ecommerce_distributed_system.order_service.entity.Order;
+import com.ecommerce_distributed_system.order_service.enums.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+    // FIND BY USER
+    List<Order> findByUserId(Long userId);
+
+
+    // FIND BY STATUS
+    List<Order> findByStatus(OrderStatus status);
+
+    // FIND BY USER + STATUS
+    List<Order> findByUserIdAndStatus(Long userId, OrderStatus status);
+
+    // FIND BY INVENTORY RESERVATION ID
+    Optional<Order> findByInventoryReservationId(Long reservationId);
+
+
+    // FIND BY PAYMENT TRANSACTION ID
+    Optional<Order> findByPaymentTransactionId(String transactionId);
+
+
+    // FETCH LATEST ORDER OF USER
+    Optional<Order> findTopByUserIdOrderByCreatedAtDesc(Long userId);
+
+}

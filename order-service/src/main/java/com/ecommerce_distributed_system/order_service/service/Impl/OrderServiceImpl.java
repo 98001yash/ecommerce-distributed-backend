568129dcf9962b301d9c6bd72ecf0986d6f376d7
98001yash <yashchauhan.gaya@gmail.com>
2026-03-18 09:49:01 +0530

    implemented the handleStockReserved method in the ordeer service impl

--- order-service/src/main/java/com/ecommerce_distributed_system/order_service/service/Impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/ecommerce_distributed_system/order_service/service/Impl/OrderServiceImpl.java
@@ -4,6 +4,10 @@ package com.ecommerce_distributed_system.order_service.service.Impl;
 import com.ecommerce_distributed_system.order_service.dtos.CreateOrderRequest;
 import com.ecommerce_distributed_system.order_service.dtos.CreateOrderResponse;
 import com.ecommerce_distributed_system.order_service.dtos.OrderResponse;
+import com.ecommerce_distributed_system.order_service.entity.Order;
+import com.ecommerce_distributed_system.order_service.enums.OrderStatus;
+import com.ecommerce_distributed_system.order_service.exception.InvalidOrderStateException;
+import com.ecommerce_distributed_system.order_service.exception.OrderNotFoundException;
 import com.ecommerce_distributed_system.order_service.repository.OrderRepository;
 import com.ecommerce_distributed_system.order_service.service.OrderService;
 import com.redditApp.events.StockExpiredEvent;
@@ -30,6 +34,56 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void handleStockReserved(StockReservedEvent event) {
+
+        log.info("Processing stockReservedEvent for orderId={}, productId={}",
+                event.getOrderId(), event.getProductId());
+
+
+        // fetch order
+        Order order = orderRepository.findById(event.getOrderId())
+                .orElseThrow(()-> {
+                    log.error("Order not found for orderId={}",event.getOrderId());
+                    return new OrderNotFoundException("order not found with id:"+event.getOrderId());
+                });
+
+        // IDEMPOTENCY CHECK
+        if (order.getStatus() == OrderStatus.INVENTORY_RESERVED) {
+
+            log.warn("Duplicate StockReservedEvent received for orderId={}",
+                    order.getId());
+
+            return; // already processed
+        }
+
+        // 3. State validation
+        if (order.getStatus() != OrderStatus.CREATED) {
+
+            log.error("Invalid state transition for orderId={}, currentStatus={}",
+                    order.getId(), order.getStatus());
+
+            throw new InvalidOrderStateException(
+                    "Cannot mark inventory reserved for order in state: " + order.getStatus()
+            );
+        }
+
+
+// 4. UPDATE ORDER
+
+        order.setStatus(OrderStatus.INVENTORY_RESERVED);
+
+//         NOTE: No reservationId in your event → skip it
+//          order.setInventoryReservationId(...)
+        orderRepository.save(order);
+
+        log.info("Order updated successfully to INVENTORY_RESERVED for orderId={}, productId={}, quantity={}",
+                order.getId(),
+                event.getProductId(),
+                event.getQuantity());
+
+
+//              5. NEXT STEP (PAYMENT - WILL ADD NEXT)
+        log.info("Inventory reserved. Ready to trigger payment for orderId={}",
+                order.getId());
 
     }
 

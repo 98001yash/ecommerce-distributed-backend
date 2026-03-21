c73fc7893c27ae86e7a21e2d29d472ee96937c90
98001yash <yashchauhan.gaya@gmail.com>
2026-03-21 08:32:42 +0530

    added the kafka topics and kafka producer in the order-service

---
+++ order-service/src/main/java/com/ecommerce_distributed_system/order_service/kafka/KafkaTopics.java
@@ -0,0 +1,8 @@
+package com.ecommerce_distributed_system.order_service.kafka;
+
+public class KafkaTopics {
+
+    public static final String ORDER_CREATED = "order-created";
+    public static final String ORDER_CANCELLED = "order-cancelled";
+    public static final String ORDER_COMPLETED = "order-completed";
+}
---
+++ order-service/src/main/java/com/ecommerce_distributed_system/order_service/kafka/OrderEventProducer.java
@@ -0,0 +1,39 @@
+package com.ecommerce_distributed_system.order_service.kafka;
+
+
+import com.redditApp.events.OrderCancelledEvent;
+import com.redditApp.events.OrderCompletedEvent;
+import com.redditApp.events.OrderCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderEventProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    //  ORDER CREATED
+    public void sendOrderCreatedEvent(OrderCreatedEvent event) {
+        log.info("Publishing OrderCreatedEvent for orderId={}", event.getOrderId());
+        kafkaTemplate.send(KafkaTopics.ORDER_CREATED, event.getOrderId().toString(), event);
+    }
+
+    // 🔥 ORDER CANCELLED
+    public void sendOrderCancelledEvent(OrderCancelledEvent event) {
+
+        log.info("Publishing OrderCancelledEvent for orderId={}", event.getOrderId());
+
+        kafkaTemplate.send(KafkaTopics.ORDER_CANCELLED, event.getOrderId().toString(), event);
+    }
+
+    // 🔥 ORDER COMPLETED
+    public void sendOrderCompletedEvent(OrderCompletedEvent event) {
+
+        log.info("Publishing OrderCompletedEvent for orderId={}", event.getOrderId());
+        kafkaTemplate.send(KafkaTopics.ORDER_COMPLETED, event.getOrderId().toString(), event);
+    }
+}

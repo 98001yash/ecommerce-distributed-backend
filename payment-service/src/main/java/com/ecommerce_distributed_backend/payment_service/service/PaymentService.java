545aa24011c9641f14e2d9e140c44304e4a846ce
98001yash <yashchauhan.gaya@gmail.com>
2026-03-22 07:42:50 +0530

    added the payment servic interface

--- payment-service/src/main/java/com/ecommerce_distributed_backend/payment_service/service/PaymentService.java
+++ payment-service/src/main/java/com/ecommerce_distributed_backend/payment_service/service/PaymentService.java
@@ -1,4 +1,29 @@
 package com.ecommerce_distributed_backend.payment_service.service;
 
+import com.ecommerce_distributed_backend.payment_service.dtos.CreatePaymentRequest;
+import com.ecommerce_distributed_backend.payment_service.dtos.PaymentResponse;
+import com.redditApp.events.StockReservedEvent;
+
 public interface PaymentService {
+
+    //  Triggered when inventory is reserved (Saga start for payment)
+    void handleStockReserved(StockReservedEvent event);
+
+    //  Create payment manually (optional API)
+    PaymentResponse createPayment(CreatePaymentRequest request);
+
+    //  Process payment (internal step)
+    void processPayment(Long paymentId);
+
+    //  Handle payment success
+    void handlePaymentSuccess(Long paymentId);
+
+    //  Handle payment failure
+    void handlePaymentFailure(Long paymentId, String reason);
+
+    //  Get payment by ID
+    PaymentResponse getPayment(Long paymentId);
+
+    //  Get payment by orderId
+    PaymentResponse getPaymentByOrderId(Long orderId);
 }

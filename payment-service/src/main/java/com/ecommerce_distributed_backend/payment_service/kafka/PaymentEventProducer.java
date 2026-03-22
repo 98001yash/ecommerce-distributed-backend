4d56b2ab0327cba6b5324cc309856bc094d942e0
98001yash <yashchauhan.gaya@gmail.com>
2026-03-22 09:41:10 +0530

    added the paymentr event producer

--- payment-service/src/main/java/com/ecommerce_distributed_backend/payment_service/kafka/PaymentEventProducer.java
+++ payment-service/src/main/java/com/ecommerce_distributed_backend/payment_service/kafka/PaymentEventProducer.java
@@ -1,4 +1,40 @@
 package com.ecommerce_distributed_backend.payment_service.kafka;
 
+import com.redditApp.events.PaymentCompletedEvent;
+import com.redditApp.events.PaymentFailedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class PaymentEventProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    //  SUCCESS EVENT
+    public void sendPaymentCompletedEvent(PaymentCompletedEvent event) {
+
+        log.info("📤 Publishing PaymentCompletedEvent → orderId={}", event.getOrderId());
+
+        kafkaTemplate.send(
+                KafkaTopics.PAYMENT_COMPLETED,
+                event.getOrderId().toString(),
+                event
+        );
+    }
+
+    //  FAILURE EVENT
+    public void sendPaymentFailedEvent(PaymentFailedEvent event) {
+
+        log.info("📤 Publishing PaymentFailedEvent → orderId={}", event.getOrderId());
+
+        kafkaTemplate.send(
+                KafkaTopics.PAYMENT_FAILED,
+                event.getOrderId().toString(),
+                event
+        );
+    }
 }

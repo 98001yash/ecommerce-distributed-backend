2a280313bd26ad3ec980a4682f1c8375f9feabb5
98001yash <yashchauhan.gaya@gmail.com>
2026-03-22 09:34:12 +0530

    added the kafka topics in the payment

---
+++ payment-service/src/main/java/com/ecommerce_distributed_backend/payment_service/kafka/KafkaTopics.java
@@ -0,0 +1,8 @@
+package com.ecommerce_distributed_backend.payment_service.kafka;
+
+
+public class KafkaTopics {
+
+    public static final String PAYMENT_COMPLETED = "payment-completed";
+    public static final String PAYMENT_FAILED = "payment-failed";
+}

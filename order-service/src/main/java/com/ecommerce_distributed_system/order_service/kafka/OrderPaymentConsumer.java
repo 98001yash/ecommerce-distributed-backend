ab7f1789844ac39ee3de82c78c5d6d713f0f2c2a
98001yash <yashchauhan.gaya@gmail.com>
2026-03-23 08:01:37 +0530

    implemented the orderPaymentconsumer

---
+++ order-service/src/main/java/com/ecommerce_distributed_system/order_service/kafka/OrderPaymentConsumer.java
@@ -0,0 +1,55 @@
+package com.ecommerce_distributed_system.order_service.kafka;
+
+
+import com.ecommerce_distributed_system.order_service.service.OrderService;
+import com.redditApp.events.PaymentCompletedEvent;
+import com.redditApp.events.PaymentFailedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderPaymentConsumer {
+
+    private final OrderService orderService;
+
+    //  PAYMENT SUCCESS
+    @KafkaListener(
+            topics = "payment-completed",
+            groupId = "order-service-group"
+    )
+    public void handlePaymentCompleted(PaymentCompletedEvent event) {
+
+        log.info("📥 Received PaymentCompletedEvent → orderId={}",
+                event.getOrderId());
+
+        try {
+            orderService.handlePaymentCompleted(event);
+        } catch (Exception ex) {
+            log.error("❌ Error processing PaymentCompletedEvent for orderId={}",
+                    event.getOrderId(), ex);
+            throw ex; // let retry happen
+        }
+    }
+
+    @KafkaListener(
+            topics = "payment-failed",
+            groupId = "order-service-group"
+    )
+    public void handlePaymentFailed(PaymentFailedEvent event) {
+
+        log.info("📥 Received PaymentFailedEvent → orderId={}",
+                event.getOrderId());
+
+        try {
+            orderService.handlePaymentFailed(event);
+        } catch (Exception ex) {
+            log.error("❌ Error processing PaymentFailedEvent for orderId={}",
+                    event.getOrderId(), ex);
+            throw ex;
+        }
+    }
+}
